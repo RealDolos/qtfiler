a5c9e5170dd883db5ddd15d0ef3aa8a5f3d1cc22
cyberia- <cyberia@cock.li>
2018-01-13 20:20:05 +0000

    upload list

--- assets/js/room.js
+++ assets/js/room.js
@@ -2,6 +2,9 @@
 var qq = require("fine-uploader/lib/core");
 var dnd = require("fine-uploader/lib/dnd");
 
+
+var uploading = {};
+
 var uploader = new qq.FineUploaderBasic({
     request: {
         endpoint: "/api/upload",
@@ -11,6 +14,35 @@ var uploader = new qq.FineUploaderBasic({
     retry: {
         enableAuto: true
     },
+    button: document.getElementById("submit"),
+    callbacks: {
+        onSubmitted: function(id, name) {
+            uploading[id] = {
+                name: name
+            };
+            var upload = document.createElement("div");
+            upload.setAttribute("id", "upload-" + id);
+            var uploadText = document.createTextNode(name + " 0%");
+            upload.appendChild(uploadText);
+            document.getElementById("uploads").appendChild(upload);
+            return true;
+        },
+        onProgress: function(id, name, uploaded, total) {
+            var fileProgress = uploading[id];
+            var progress = Math.round(100 * uploaded / total);
+            var uploadText = document.createTextNode(fileProgress.name + " " + progress + "%");
+            var upload = document.getElementById("upload-" + id);
+            if (upload.firstChild) {
+                upload.removeChild(upload.firstChild);
+            }
+            upload.appendChild(uploadText);
+        },
+        onComplete: function(id, name, response, xhr) {
+            var upload = document.getElementById("upload-" + id);
+            document.getElementById("uploads").removeChild(upload);
+            delete uploading[id];
+        }
+    }
 });
 
 var dragAndDrop = new dnd.DragAndDrop({
@@ -26,6 +58,5 @@ var dragAndDrop = new dnd.DragAndDrop({
         }
     }
 });
-
 
 console.log("haha");
