a03a91c216b5cb0588d188188c0c4746dbd663c4
cyberia- <cyberia@cock.li>
2018-02-25 15:57:29 +0000

    use max file length

--- lib/qtfile_web/controllers/file_controller.ex
+++ lib/qtfile_web/controllers/file_controller.ex
@@ -31,7 +31,7 @@ defmodule QtfileWeb.FileController do
 
   defp save_file_loop(conn, state, write) do
     {status, data, conn} = read_body(conn,
-      length: Application.get_env(:qtfile, QtfileWeb.Endpoint, :max_file_length),
+      length: Settings.get_setting_value!("max_file_length"),
       read_length: 64 * 1024,
       read_timeout: 1024
     )
