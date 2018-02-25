2ec45e07750479560d127f31b82052124c7abfff
cyberia- <cyberia@cock.li>
2018-02-25 00:25:03 +0000

    work around stupid bug

--- lib/qtfile_web/controllers/admin_controller.ex
+++ lib/qtfile_web/controllers/admin_controller.ex
@@ -10,12 +10,12 @@ defmodule QtfileWeb.AdminController do
   end
 
   def set_settings(conn, params) do
+    params = Map.delete(params, "_csrf_token")
     result = sequence(
       Enum.map(params, fn({k, v}) ->
-        monad Right do
-          setting <- get_setting_by_key(k) |> nilToEither() |> tagLeft(:setting_not_found)
-          _ <- update_setting(setting, %{value: v}) |> errToEitherTag(:could_not_update)
-          return nil
+        get_setting_by_key(k) |> nilToEither() |> tagLeft(:setting_not_found) >>>
+        fn(setting) ->
+          update_setting(setting, %{value: v}) |> errToEitherTag(:could_not_update)
         end
       end)
     )
