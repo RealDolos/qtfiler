63a81446963d90f7f9791e944346a7e32b566622
cyberia- <cyberia@cock.li>
2018-03-03 19:48:51 +0000

    upload states

--- lib/qtfile/file_processing/upload_state.ex
+++ lib/qtfile/file_processing/upload_state.ex
@@ -1,3 +1,19 @@
 defmodule Qtfile.FileProcessing.UploadState do
+  use GenServer
 
+  def start_link() do
+    GenServer.start_link(__MODULE__, {})
+  end
+
+  def init({}) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:put, k, v}, _from, state) do
+    {:reply, :ok, Map.put(state, k, v)}
+  end
+
+  def handle_call({:get, k}, _from, state) do
+    {:reply, Map.fetch(state, k), state}
+  end
 end
