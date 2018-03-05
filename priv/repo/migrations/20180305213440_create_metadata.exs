1d584ce6050fc16e2721f2370b9b806d51c5fd61
cyberia- <cyberia@cock.li>
2018-03-05 21:37:26 +0000

    metadata table

---
+++ priv/repo/migrations/20180305213440_create_metadata.exs
@@ -0,0 +1,12 @@
+defmodule Qtfile.Repo.Migrations.CreateMetadata do
+  use Ecto.Migration
+
+  def change do
+    create table(:metadata) do
+      add :file_id, references(:files)
+      add :data, :map
+    end
+
+    create unique_index(:metadata, [:file_id])
+  end
+end
