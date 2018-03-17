19e6feeff4312203177ed791ab00253b46e43b54
cyberia- <cyberia@cock.li>
2018-03-17 00:40:32 +0000

    Revert "at request of beta testers: thumbnails go 100 px above cursor"
    This reverts commit 7a8d8d000eeefa9383de84c6d2f5417ae786fb0f.

--- assets/js/file-list-view.js
+++ assets/js/file-list-view.js
@@ -69,8 +69,8 @@ export default function(room) {
         },
         methods: {
             mouseMove(e) {
-                this.mouse.x = e.pageX - 100;
-                this.mouse.y = e.pageY - 100;
+                this.mouse.x = e.pageX + 1;
+                this.mouse.y = e.pageY + 1;
             },
             async wake() {
                 return await this.$data.wakeUploader();
